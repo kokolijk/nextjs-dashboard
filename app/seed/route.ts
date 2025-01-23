1786907eaf7dafe313704630b55c31d29c709605
joakim.li@ciicsh.com <joakim.li@ciicsh.com>
2025-01-23 11:07:48 +0800

    bcrypt

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
